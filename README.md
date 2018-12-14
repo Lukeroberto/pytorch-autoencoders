6e4d071b42c0c527f4f7d06d548657f128f97118
luke <lukeroberto213@gmail.com>
2018-12-14 11:21:38 -0500

    Initial commit of first autoencoder

---
+++ README.md
@@ -0,0 +1,12 @@
+## Autoencoder Study
+Purpose of this repository is to study a variety of autoencoders in 
+different domains to understand how they work and what they are good for.
+
+Types:
+    Fully Connected Vanilla Autoencoder
+    Convolutional Vanilla Autoencoder
+    Variational Autoencoder
+
+
+Results:
+    Currently just tests reconstruction ability on mnist
---
+++ fully_connected_autoencoder.py
@@ -0,0 +1,102 @@
+import os
+import torch
+from torch.autograd import Variable
+import torch.nn as nn
+import torch.nn.functional as F
+from torch.autograd import Variable
+import torchvision.datasets as dset
+import torchvision.transforms as transforms
+import matplotlib.pyplot as plt
+import numpy as np
+
+root = './data'
+if not os.path.exists(root):
+    os.mkdir(root)
+
+img = './results/fully_connected'
+if not os.path.exists(img):
+    os.mkdir(img)
+
+trans = transforms.Compose([transforms.ToTensor(), transforms.Normalize((0.5,), (1.0,))])
+
+train_set = dset.MNIST(root=root, train=True, transform=trans, download=True)
+test_set  = dset.MNIST(root=root, train=False, transform=trans, download=True)
+
+batch_size = 100
+
+train_loader = torch.utils.data.DataLoader(
+                dataset=train_set, 
+                batch_size=batch_size,
+                shuffle=True)
+
+test_loader = torch.utils.data.DataLoader(
+                dataset=test_set, 
+                batch_size=batch_size,
+                shuffle=True)
+
+
+# Network
+class AutoEncoder(nn.Module):
+    def __init__(self):
+        super(AutoEncoder, self).__init__()
+
+        # 3 passes down to 100 dims
+        # 3 passes back up to 28*28
+        self.fc1 = nn.Linear(28*28, 500)
+        self.fc2 = nn.Linear(500, 256)
+        self.fc3 = nn.Linear(256, 100)
+
+        self.fc4 = nn.Linear(100, 256)
+        self.fc5 = nn.Linear(256, 500)
+        self.fc6 = nn.Linear(500, 28*28)
+    
+    def forward(self, x):
+        x = x.view(-1, 28*28)
+        x = F.relu(self.fc1(x))
+        x = F.relu(self.fc2(x))
+        x = F.relu(self.fc3(x))
+        x = F.relu(self.fc4(x))
+        x = F.relu(self.fc5(x))
+        x = self.fc6(x)
+        return x
+
+    def name(self):
+        return "AutoEncoder"
+
+
+model = AutoEncoder()
+
+optimizer = torch.optim.Adam(model.parameters(), lr=0.01)
+
+def mse_loss(input, target):
+    return torch.sum((input - target) ** 2)
+
+for epoch in range(10):
+
+    # Training
+    print("Training")
+    for batch_idx, (x, target) in enumerate(train_loader):
+        optimizer.zero_grad()
+
+        x = Variable(x)
+        output = model(x)
+
+        loss  = mse_loss(output, x.view(-1, 28*28))
+        
+        loss.backward()
+        optimizer.step()
+
+        if (batch_idx+1) % 100 == 0 or (batch_idx+1) == len(train_loader):
+            print("Epoch: {}, Batch index: {}, Train Loss: {:.6f}".format(epoch, batch_idx+1, loss))
+    
+
+    print("Train Results")
+    fig, (ax1, ax2) = plt.subplots(1, 2)
+    
+    input = x.view(-1, 28, 28, 1).detach().numpy()
+    out = model(x).view(-1, 28, 28, 1).detach().numpy()
+
+    ax1.imshow(np.squeeze(input[0, :, :, :]))
+    ax2.imshow(np.squeeze(out[0, :, :, :]))
+    
+    plt.savefig(img + "/train_target_{}_epoch_{}.png".format(target[0],epoch))
