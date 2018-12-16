e97a124bc3f74b80f6ab62fe4e423ba87d9e3c2c
luke <lukeroberto213@gmail.com>
2018-12-15 19:04:45 -0500

    Finished variational autoencoder

---
+++ variational_autoencoder.py
@@ -0,0 +1,120 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import os
+
+import torch
+import torch.utils.data
+from torch import nn, optim
+from torch.nn import functional as F
+from torchvision import datasets, transforms
+from torchvision.utils import save_image
+
+root = './data'
+if not os.path.exists(root):
+    os.mkdir(root)
+
+images = './results/variational'
+if not os.path.exists(images):
+    os.mkdir(images)
+
+torch.manual_seed(1337)
+batch_size = 128
+
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+train_loader = torch.utils.data.DataLoader(
+    datasets.MNIST(root, train=True, download=True,
+                   transform=transforms.ToTensor()),
+    batch_size=batch_size, shuffle=True)
+
+class VAE(nn.Module):
+    def __init__(self):
+        super(VAE, self).__init__()
+
+        self.fc1 = nn.Linear(784, 400)
+        self.fc21 = nn.Linear(400, 20)
+        self.fc22 = nn.Linear(400, 20)
+        self.fc3 = nn.Linear(20, 400)
+        self.fc4 = nn.Linear(400, 784)
+
+    def encode(self, x):
+        h1 = F.relu(self.fc1(x))
+        return self.fc21(h1), self.fc22(h1)
+
+    def reparameterize(self, mu, logvar):
+        std = torch.exp(0.5*logvar)
+        eps = torch.randn_like(std)
+        return eps.mul(std).add_(mu)
+
+    def decode(self, z):
+        h3 = F.relu(self.fc3(z))
+        return torch.sigmoid(self.fc4(h3))
+
+    def forward(self, x):
+        mu, logvar = self.encode(x.view(-1, 784))
+        z = self.reparameterize(mu, logvar)
+        return self.decode(z), mu, logvar
+
+    def name(self):
+        return "VariationalAutoEncoder"
+
+
+model = VAE().to(device)
+optimizer = optim.Adam(model.parameters(), lr=1e-3)
+
+
+# Reconstruction + KL divergence losses summed over all elements and batch
+def loss_function(recon_x, x, mu, logvar):
+    BCE = F.binary_cross_entropy(recon_x, x.view(-1, 784), reduction='sum')
+
+    # see Appendix B from VAE paper:
+    # Kingma and Welling. Auto-Encoding Variational Bayes. ICLR, 2014
+    # https://arxiv.org/abs/1312.6114
+    # 0.5 * sum(1 + log(sigma^2) - mu^2 - sigma^2)
+    KLD = -0.5 * torch.sum(1 + logvar - mu.pow(2) - logvar.exp())
+
+    return BCE + KLD
+
+
+def train(epoch):
+    model.train()
+    train_loss = 0
+    for batch_idx, (data, _) in enumerate(train_loader):
+
+        data = data.to(device)
+
+        optimizer.zero_grad()
+
+        recon_batch, mu, logvar = model(data)
+
+        loss = loss_function(recon_batch, data, mu, logvar)
+        loss.backward()
+        train_loss += loss.item()
+
+        optimizer.step()
+        if batch_idx+1 % 100 == 0:
+            print('Train Epoch: {} [{}/{} ({:.0f}%)]\tLoss: {:.6f}'.format(
+                epoch, batch_idx * len(data), len(train_loader.dataset),
+                100. * batch_idx / len(train_loader),
+                loss.item() / len(data)))
+
+    print('====> Epoch: {} Average loss: {:.4f}'.format(epoch, train_loss / len(train_loader.dataset)))
+
+    fig, (ax1, ax2) = plt.subplots(1,2)
+    input = data.view(-1, 28, 28, 1).detach()
+    out = model(data)[0].view(-1, 28, 28, 1).detach()
+
+    input = input.cpu()
+    out = out.cpu()
+
+
+    ax1.imshow(np.squeeze(input[0, :, :, :]))
+    ax2.imshow(np.squeeze(out[0, :, :, :]))
+
+    plt.savefig(images + "/vae_epoch_{}.png".format(epoch))
+
+
+# Training period
+for ep in range(0,11):
+    print("Training... Epoch: {}".format(ep))
+    train(ep)
